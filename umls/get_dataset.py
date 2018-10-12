996b02b1450895f066a87f596c74d9c042a0b9e0
Neha Nayak <nnayak@umass.edu>
2018-10-11 21:04:20 -0400

    some data vis stuff

--- umls/get_dataset.py
+++ umls/get_dataset.py
@@ -1,42 +1,69 @@
+import sys
 import umls_lib
 import sqlite3
 
 def comma_join_strings(items):
   return ", ".join("'" + item + "'" for item in items)
 
+def read_config(relation_types_string, fine_relation_types_string,
+    included_sources_string, excluded_sources_string):
+  return [x.split("|") if x != "None" else None
+      for x in [relation_types_string, fine_relation_types_string,
+        included_sources_string,excluded_sources_string]]
+
+def read_subgraph_from_config_line(config_line):
+  fields = config_line.strip().split("\t")
+  (relation_types, fine_relation_types, included_sources,
+      excluded_sources) = read_config(*fields[1:5])
+
+  if fields[5] != "None":
+    type_prefix = fields[5]
+  else:
+    type_prefix = ""
+  return UMLSSubgraph(relation_types, fine_relation_types,
+      included_sources, excluded_sources, type_prefix)
+
+
 class UMLSSubgraph(object):
   def __init__(self, relation_types, fine_relation_types, included_sources,
-      excluded_sources, type_prefix=""):
+      excluded_sources, subgraph_name, type_prefix=""):
     """
       Args:
-        relation_types: at most one relation type
+        relation_types: a list of relation types. If None, all relations are
+        included
         fine_relation_types: at most one fine relation type; overrides
         relation_types.
         included_sources: a list of sources to be included. If None, all sources are
         included.
         excluded_sources: a list of sources to be excluded. If None, ignored
+        subgraph_name: a name for the subgraph in the db
         type_prefix: semantic type as a prefix of the semantic tree type
     """
+
+    # TODO: Change the 'IN's to 'LIKE's when possible
     if fine_relation_types is not None:
       assert relation_types is None
     self.fine_relation_types = fine_relation_types
-    self.relation_types = relation_types
+    if relation_types is None:
+      self.relation_types = umls_lib.ALL_RELATION_TYPES
+    else:
+      self.relation_types = relation_types
 
     if included_sources is None:
-      included_sources = umls_lib.all_sources
+      included_sources = umls_lib.ALL_SOURCES
     if excluded_sources is None:
       self.sources = set(included_sources)
     else:
       self.sources = set(included_sources) - set(excluded_sources)
 
-    self.type_prefix = type_prefix  
+    self.type_prefix = type_prefix
 
   def get_pairs(self, conn):
     """
       Args:
         conn: Database connection
     """
-    select_string = "SELECT DISTINCT cui1, cui2 FROM RELATIONS WHERE "
+    select_string = "SELECT DISTINCT cui1, cui2, relation, fine_relation, source FROM RELATIONS WHERE "
     select_string += "source IN ({})".format(
       comma_join_strings(self.sources))
     if self.fine_relation_types is not None:
@@ -47,29 +74,20 @@ class UMLSSubgraph(object):
               and self.relation_types is not None)
       select_string += " AND relation IN ({})".format(
         comma_join_strings(self.relation_types))
-    print(select_string)
     c = conn.cursor()
     c.execute(select_string)
     for i in c.fetchall():
-      print i
+      print "\t".join(list(i))
 
 
 def main():
   conn = sqlite3.connect(umls_lib.DB_FILE)
   c = conn.cursor()
 
-  subgraph = UMLSSubgraph(relation_types=['PAR'],
-                          fine_relation_types=None,
-                          included_sources=None,
-                          excluded_sources=["MTH"])
-  subgraph.get_pairs(conn)
-
-  #c.execute(
-  #"SELECT DISTINCT cui1, cui2 FROM relations WHERE source in ('MSH', 'RXNORM') AND relation in ('PAR')")
-  #for i in c.fetchall():
-  #  print i
-  conn.close()
-
+  with open(sys.argv[1], 'r') as f:
+    for line in f:
+      subgraph = read_subgraph_from_config_line(line)
+      subgraph.get_pairs(conn)
   pass
 
 if __name__=="__main__":
--- umls/umls_lib.py
+++ umls/umls_lib.py
@@ -4,7 +4,14 @@ CONCEPT_FILE = "META/MRCONSO.RRF"
 RELATION_FILE = "META/MRREL.RRF"
 SEMTYPE_FILE = "META/MRSTY.RRF"
 
-all_sources = ["MSH", "RXNORM", "MTH", "MONDO"]
+ALL_RELATION_TYPES = ["RO", "RB", "RN", "PAR", "CHD"]
+
+ALL_SOURCES = ["AIR", "AOD", "AOT", "ATC" "CCS",
+"CCS_10", "CSP", "CST", "CVX", "FMA", "GO", "HCPCS", "HGNC", "HL7V2.5", "HL7V3.0",
+"HPO", "ICD10PCS", "ICD9CM", "ICPC", "LCH_NW", "LNC", "MEDLINEPLUS", "MSH",
+"MTH", "MTHHH", "MTHICD9", "MTHMST", "MTHSPL", "MVX", "NCBI",  "NCI", "NDFRT",
+"NDFRT_FDASPL", "NDFRT_FMTSME", "OMIM", "PDQ", "RAM", "RXNORM", "SOP", "SRC",
+"TKMT", "USPMG", "UWDA", "VANDF"]
 
 def read_file(filename):
   with open(filename, 'r') as f:
