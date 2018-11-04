86d58a8387e575ec5f8bccf5bc9ea4ddfb57ff9f
NayakNeha <nnayak@umass.edu>
2018-11-04 13:23:01 -0500

    populating graph data structure

--- oaei/get_subclass_tree.py
+++ oaei/get_subclass_tree.py
@@ -1,41 +1,21 @@
+import pickle
 import sys
 import rdflib
-import ontospy
-
-class RDFPredicates(object):
-  SYNTAX_FIRST = "http://www.w3.org/1999/02/22-rdf-syntax-ns#first"
-  SYNTAX_REST = "http://www.w3.org/1999/02/22-rdf-syntax-ns#rest"
-  SYNTAX_TYPE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
-  DOMAIN = "http://www.w3.org/2000/01/rdf-schema#domain"
-  LABEL = "http://www.w3.org/2000/01/rdf-schema#label"
-  RANGE = "http://www.w3.org/2000/01/rdf-schema#range"
-  SUBCLASS_OF = "http://www.w3.org/2000/01/rdf-schema#subClassOf"
-  DISJOINT_WITH = "http://www.w3.org/2002/07/owl#disjointWith"
-  HAS_VALUE = "http://www.w3.org/2002/07/owl#hasValue"
-  MIN_CARDINALITY = "http://www.w3.org/2002/07/owl#minCardinality"
-  ON_PROPERTY = "http://www.w3.org/2002/07/owl#onProperty"
-  ONE_OF = "http://www.w3.org/2002/07/owl#oneOf"
-  UNION_OF = "http://www.w3.org/2002/07/owl#unionOf"
-
-class OAEINode(object):
-  def __init__(self, uid, label):
-    self.uid = uid
-    self.label = label
-    self.subclasses = []
-    pass
+import oaei_lib
 
 def main():
   owl_file = sys.argv[1]
+  output_file = owl_file + ".graph.pkl"
   g = rdflib.Graph()
   result = g.parse(owl_file)
   oaei_nodes = {}
 
   for subj, pred, obj in g:
-    if str(pred) == RDFPredicates.LABEL:
-      oaei_nodes[str(subj)] = OAEINode(str(subj), str(obj))
+    if str(pred) == oaei_lib.RDFPredicates.LABEL:
+      oaei_nodes[str(subj)] = oaei_lib.OAEINode(str(subj), str(obj))
 
   for subj, pred, obj in g:
-    if str(pred) == RDFPredicates.SUBCLASS_OF:
+    if str(pred) == oaei_lib.RDFPredicates.SUBCLASS_OF:
       if str(subj) in oaei_nodes and str(obj) in oaei_nodes:
         oaei_nodes[str(obj)].subclasses.append(oaei_nodes[str(subj)])
 
@@ -44,9 +24,15 @@ def main():
     for y in sum([x.subclasses for x in oaei_nodes.values()], [])])
   non_subclass_nodes = all_nodes - subclass_nodes
 
-  for node_id, node in oaei_nodes.items():
-    for subclass in node.subclasses:
-      print(node.uid+"\t"+subclass.uid)
+  root_node = oaei_lib.OAEINode("ROOT", "ROOT")
+  root_node.subclasses = [oaei_nodes[non_sub_node] for non_sub_node in
+      non_subclass_nodes]
+  oaei_nodes["ROOT"] = root_node
+
+  graph = oaei_lib.OAEIGraph(oaei_nodes, root_node)
+  with open(output_file, 'wb') as f:
+    pickle.dump(graph, f)
+
 
 if __name__ == "__main__":
   main()
