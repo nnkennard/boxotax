8104ff2bbec29452c82ec01d131ffcfe739899fc
NayakNeha <nnayak@umass.edu>
2018-11-06 14:35:14 -0500

    output graph as pickle

--- oaei/get_subclass_tree.py
+++ oaei/get_subclass_tree.py
@@ -17,16 +17,14 @@ def main():
   for subj, pred, obj in g:
     if str(pred) == oaei_lib.RDFPredicates.SUBCLASS_OF:
       if str(subj) in oaei_nodes and str(obj) in oaei_nodes:
-        oaei_nodes[str(obj)].subclasses.append(oaei_nodes[str(subj)])
+        oaei_nodes[str(obj)].subclasses.append(str(subj))
 
-  all_nodes = set([y.uid for y in oaei_nodes.values()])
-  subclass_nodes = set([y.uid
-    for y in sum([x.subclasses for x in oaei_nodes.values()], [])])
+  all_nodes = set(oaei_nodes.keys())
+  subclass_nodes = set(sum([x.subclasses for x in oaei_nodes.values()], []))
   non_subclass_nodes = all_nodes - subclass_nodes
 
   root_node = oaei_lib.OAEINode("ROOT", "ROOT")
-  root_node.subclasses = [oaei_nodes[non_sub_node] for non_sub_node in
-      non_subclass_nodes]
+  root_node.subclasses = list(non_subclass_nodes)
   oaei_nodes["ROOT"] = root_node
 
   graph = oaei_lib.OAEIGraph(oaei_nodes, root_node)
