ece7d0a18a3ba9bc71b17551a876774d0ba18f35
Neha Nayak <nnayak@umass.edu>
2018-11-10 19:40:40 -0500

    a lot of moving stuff around

--- affinity/base_sim.py
+++ affinity/base_sim.py
@@ -1,7 +1,10 @@
 import nltk
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet
 import re
 
 CAMEL_CASE_SPLITTER = re.compile('([A-Z][a-z]+)')
+ENGLISH_STOP_WORDS = stopwords.words('english')
 
 def treat_string(input_string):
   camel_case_split = re.split(CAMEL_CASE_SPLITTER, input_string)
@@ -20,13 +23,25 @@ def treat_string(input_string):
 
   return " ".join(atoms)
 
+def remove_stopwords(input_string):
+  return " ".join([word for word in input_string.split()
+    if word not in ENGLISH_STOP_WORDS])
+
+def get_definitions(label):
+  definitions = []
+  for word in label.split():
+    print("WWWWW "+word)
+    synsets = wordnet.synsets(word)
+    if synsets:
+      definitions += synsets[0].definition().split()
+  return definitions
 
 
 class Concept(object):
   def __init__(self, label, definition):
     self.label = label
     self.definition = definition
-    self.normalize()
+    #self.normalize()
     pass
 
   def normalize(self):
@@ -47,11 +62,21 @@ def base_sim(main_concept, aux_concept):
   main_label = remove_stopwords(main_label)
   aux_label = remove_stopwords(aux_label)
 
+  main_definitions = get_definitions(main_label)
+  print(main_definitions)
+  aux_definitions = get_definitions(aux_label)
+  print(aux_definitions)
+
 def main():
   print(treat_string("OnlyGoBackwards"))
   print(treat_string("onlyGoBackwards"))
   print(treat_string("only-go-backwards"))
   print(treat_string("only_go_backwards"))
+
+  main_concept = Concept("CleanCanteen", "o")
+  aux_concept = Concept("SplendidPlant", "p")
+
+  print base_sim(main_concept, aux_concept)
   pass
 
 if __name__ == "__main__":
