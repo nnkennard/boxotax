f5a4e507e587d15b639ddda12bb9598c479696a5
Neha Nayak <nnayak@umass.edu>
2018-12-07 13:08:00 -0500

    refactor

--- models/train.py
+++ models/train.py
@@ -15,6 +15,12 @@ flags.DEFINE_integer('num_epochs', 10, 'number of epochs (max)')
 flags.DEFINE_integer('random_seed', 43, 'value for the random seed')
 flags.DEFINE_string('train_path', None,
     'path to train conditional probabilities')
+flags.DEFINE_string('load_path', None,
+    'path to train conditional probabilities')
+flags.DEFINE_string('save_path', None,
+    'path to train conditional probabilities')
+flags.DEFINE_integer('save_freq', 5,
+    'path to train conditional probabilities')
 flags.DEFINE_integer('batch_size', 32, 'batch size')
 flags.DEFINE_integer('embedding_size', 20,
     'total size of embedding (including max and min)')
@@ -28,54 +34,35 @@ flags.DEFINE_boolean('verbose', False, 'Whether to print batch loss')
 # For some reason, pytorch crashes with more workers
 NUM_WORKERS = 0
 
-def get_train_and_dev_sets(train_path):
-  train_ds = box_lib.BoxDataset(train_path)
+def setup():
+  box_lib.set_random_seed(FLAGS.random_seed)
+
+
+
+def get_data():
+  train_ds = box_lib.BoxDataset(FLAGS.train_path)
   train_dl = DataLoader(train_ds, batch_size=FLAGS.batch_size,
       shuffle=True, num_workers=NUM_WORKERS)
   dev_ds = box_lib.BoxDataset(
-      train_path.replace("train", "dev"))
+      FLAGS.train_path.replace("train", "dev"))
   dev_dl = DataLoader(dev_ds, batch_size=FLAGS.batch_size,
       shuffle=True, num_workers=NUM_WORKERS)
-
   return train_ds, train_dl, dev_ds, dev_dl
 
-
-def main():
-
-  box_lib.set_random_seed(FLAGS.random_seed)
-
-  train_ds, train_dl, dev_ds, dev_dl = get_train_and_dev_sets(FLAGS.train_path)
-
+def get_and_maybe_load_model(train_ds):
   model = box_lib.Boxes(train_ds.vocab_size, FLAGS.embedding_size)
   model.to(FLAGS.device)
   criterion = nn.MSELoss()
   optimizer = torch.optim.Adam(model.parameters(), lr=FLAGS.learning_rate)
+  return model, criterion, optimizer
 
+
+def run_train_iters(model, criterion, optimizer,
+    train_dl, dev_dl, train_ds, dev_ds):
   for epoch in range(FLAGS.num_epochs):
     print(epoch)
 
-    model.train()
-    running_loss = 0.0
-
-    for X, y in train_dl:
-      torch.cuda.empty_cache()
-      X, y = X.to(FLAGS.device), y.to(FLAGS.device)
-
-      #TODO: Use more canonical regularization maybe
-      with torch.set_grad_enabled(True):
-        y_, norms = model(X)
-        loss = criterion(y_, y)
-        loss += FLAGS.l2_lambda * norms
-        running_loss += loss.item() * X.shape[0]
-
-        # Statistics
-        if FLAGS.verbose:
-          print("    batch loss: "+str(loss.item()))
-
-
-      optimizer.zero_grad()
-      loss.backward()
-      optimizer.step()
+    running_loss = run_train_iter(model, criterion, optimizer, train_dl)
 
     if epoch % 1 == 0:
       #print box_lib.check_cond_probs(train_ds, model)
@@ -84,10 +71,53 @@ def main():
 
 
     print("  Train Loss: "+str(running_loss / len(train_dl.dataset)))
-  for a, b, c in zip(train_ds.X_train, model(train_ds.X_train)[0] ,
+
+
+def run_train_iter(model, criterion, optimizer, train_dl):
+  model.train()
+  running_loss = 0.0
+
+  for X, y in train_dl:
+    torch.cuda.empty_cache()
+    X, y = X.to(FLAGS.device), y.to(FLAGS.device)
+
+    #TODO: Use more canonical regularization maybe
+    with torch.set_grad_enabled(True):
+      y_, norms = model(X)
+      loss = criterion(y_, y)
+      loss += FLAGS.l2_lambda * norms
+      running_loss += loss.item() * X.shape[0]
+
+      # Statistics
+      if FLAGS.verbose:
+        print("    batch loss: "+str(loss.item()))
+
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return running_loss
+
+
+def finish_train(train_ds, model):
+  for a, b, c in zip(train_ds.X_train, model(train_ds.X_train)[0],
       box_lib.check_cond_probs(train_ds, model)):
-      print "\t".join([str(a.data.tolist()[0]),
-        str(a.data.tolist()[1]), str(b.item()), c])
+    print "\t".join([str(a.data.tolist()[0]), str(a.data.tolist()[1]), str(b.item()), c])
+    pass
+
+
+def main():
+
+  train_ds, train_dl, dev_ds, dev_dl = get_data()
+
+  model, criterion, optimizer = get_and_maybe_load_model(train_ds)
+
+  run_train_iters(model, criterion, optimizer, train_dl, dev_dl, train_ds,
+      dev_ds)
+
+  finish_train(train_ds, model)
+
 
 if __name__ == "__main__":
   FLAGS(sys.argv)
