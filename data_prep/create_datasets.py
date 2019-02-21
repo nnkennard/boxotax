186e9c95cdc204b487aabd684bf8b3538511ba7b
Neha Nayak <nnayak@umass.edu>
2019-02-21 16:36:22 -0500

    closer to being done

--- data_prep/create_datasets.py
+++ data_prep/create_datasets.py
@@ -1,4 +1,5 @@
 import collections
+import sys
 import time
 import tqdm
 
@@ -7,24 +8,33 @@ def find_offset(num_nodes):
   return 10 ** num_digits
 
 class Graph(object):
-  def __init__(self, pairs):
-    self.graph, self.nodes = self.graph_from_pairs(pairs)
+  def __init__(self, file_name):
+    self.graph, self.probs, self.nodes = self.graph_from_file(file_name)
 
-  def graph_from_pairs(self, pair_list):
+  def graph_from_file(self, file_name):
     graph = collections.defaultdict(list)
+    probs = collections.defaultdict(dict)
     nodes = set()
-    for hypo, hyper in pair_list:
-      graph[hyper].append(hypo)
-      nodes.update([hypo, hyper])
-    return graph, nodes
 
-  def add_to_vocab(offset):
+    with open(file_name, 'r') as f:
+      for line in f:
+        hypo, hyper, prob = line.strip().split("\t")
+        graph[hyper].append(hypo)
+        probs[hypo][hyper] = prob
+        nodes.update([hypo, hyper])
+    return graph, probs, sorted(list(nodes))
+
+  def add_to_vocab(self, vocab, offset):
+    indices = [0]
     for i, node in enumerate(sorted(self.nodes)):
       if i == 0:
         assert node == "!!ROOT"
         vocab[0] = node
       else:
-        vocab[offset + i] = node
+        index = offset + i
+        vocab[index] = node
+        indices.append(index)
+    return sorted(indices)
 
 
 class SimilarityFunction(object):
@@ -35,56 +45,69 @@ class SimilarityFunction(object):
 
 
 class DatasetPair(object):
-  def __init__(self, main_pairs, aux_pairs, similarity_fn, alignment=None):
-    main_graph = Graph(main_pairs)
-    aux_graph = Graph(aux_pairs)
+  def __init__(self, main_file, aux_file, similarity_fn, alignment=None):
+    main_graph = Graph(main_file)
+    aux_graph = Graph(aux_file)
     if len(main_graph.nodes) < len(aux_graph.nodes):
       main_graph, aux_graph = aux_graph, main_graph
     self.main_graph = main_graph
     self.aux_graph = aux_graph
-    self.vocab = self.get_vocab()
+    (self.vocab, self.main_indices,
+        self.aux_indices) = self.get_combined_vocab()
 
     if alignment is not None:
       self.aligned_pairs, self.inter_negatives = self.get_alignment_from_file()
     else:
-      self.aligned_pairs, self.inter_negatives =
-      self.get_alignment_from_similarity(similarity_func)
+      (self.aligned_pairs,
+          self.inter_negatives) = self.get_alignment_from_similarity(
+              similarity_func)
 
-
-  def get_vocab(self):
+  def get_combined_vocab(self):
     vocab = {}
-    offset = find_offset(len(main_graph.nodes))
-    self.aux_graph.add_to_vocab(vocab, offset=0)
-    self.main_graph.add_to_vocab(vocab, offset=offset)
+    offset = find_offset(len(self.main_graph.nodes))
+    aux_indices = self.aux_graph.add_to_vocab(vocab, offset=0)
+    main_indices = self.main_graph.add_to_vocab(vocab, offset=offset)
+    return vocab, main_indices, aux_indices
 
-
-# Delete this
-class DatasetPair(object):
-  def __init__(self, main_dataset, aux_dataset, similarity_func,
-      alignment_info=None):
-    #self.main_dataset = main_dataset
-    #self.aux_dataset = aux_dataset
-
-    self.main_graph = Graph(main_dataset)
-    self.aux_graph = Graph(aux_dataset)
-  def get_aligned_pairs(self, similarity_func):
+  def get_alignment_from_similarity(self, similarity_func):
     aligned_pairs = []
     negative_pairs = []
-    for main_node in main_dataset.nodes:
-      for aux_node in aux_dataset.nodes:
-        sim = similarity_func.func(main_node.label, aux_node.label)
+    for main_idx in self.main_indices:
+      for aux_idx in self.aux_indices:
+        sim = similarity_func.func(vocab[main_idx], vocab[aux_idx])
         if sim > similarity_func.align_threshold:
-          aligned_pairs.append((main_node, aux_node))
+          aligned_pairs.append((main_idx, aux_idx))
         elif sim < similarity_func.unrelated_threshold:
-          negative_pairs.append((main_node, aux_node))
+          negative_pairs.append((main_idx, aux_idx))
 
+    return aligned_pairs, negative_pairs
 
+  def print_probabilities():
+    # Print main probabilities
+    for hyper, hypos in main_graph.graph:
+      for hypo in hypos:
+        prob = main_graph.probs[hypo][hyper]
+        print_line(handle, hypo, hyper, prob, 1.0)
+    # Print aux probabilities
+    for hyper, hypos in aux_graph.graph:
+      for hypo in hypos:
+        prob = main_graph.probs[hypo][hyper]
+        print_line(handle, hypo, hyper, prob, 1.0)
+
+    for n1, n2 in self.inter_negatives:
+      print_line(handle, n1, n2, 0.0, 0.0)
+    for n1, n2 in self.alignments:
+      print_line(handle, n1, n2, 1.0, 1.0)
+
+  def print_vocab():
     pass
 
-  def get_inter_negatives(self):
+  def print_dataset(filename):
+    pass
 
 def main():
-  pass
+ file_1, file_2 = sys.argv[1:3]
+ k = DatasetPair(file_1, file_2, None, None)
 
 if __name__ == "__main__":
   main()
